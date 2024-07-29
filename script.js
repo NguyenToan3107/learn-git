a783a2a0a7192c4937528daea484bfbac48f0f26
NguyenToan3107 <nguyentoan31072k3@gmail.com>
2024-07-29 23:24:25 +0700

    init project

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const mainImage = document.querySelector('.main-image');
+    const thumbnails = document.querySelectorAll('.thumbnail');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            // Lưu ảnh chính hiện tại
+            let tempSrc = mainImage.src;
+            // Đổi ảnh chính thành ảnh phụ được click
+            mainImage.src = thumbnail.src;
+            // Đổi ảnh phụ thành ảnh chính cũ
+            thumbnail.src = tempSrc;
+        });
+    });
+});
